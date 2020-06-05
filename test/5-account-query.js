2f9e70a3affed73817048795aa3651661bf17ed5
ashish chatterjee <ashish@kulfi.io>
2020-06-05 15:13:56 -0700

    updated account query test

--- test/5-account-query.js
+++ test/5-account-query.js
@@ -30,11 +30,11 @@ describe("Account Query", () => {
             const res = await request(app).get("/api").send({ query: byId });
 
             const data = JSON.parse(res.text).data.accounts;
-            const admin = data.filter(x => x.accountid == 2)[0];
-            const basic = data.filter(x => x.accountid == 1)[0];
+            const admin = data.filter((x) => x.accountid == 2)[0];
+            const basic = data.filter((x) => x.accountid == 1)[0];
 
             expect(res.status).equal(200);
-            expect(data).to.be.an("array")
+            expect(data).to.be.an("array");
             expect(basic.firstname).equal("Basic - updated");
             expect(admin.role.display).equal("Admin User");
         });
